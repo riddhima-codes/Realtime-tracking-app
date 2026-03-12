0406ba8427de2068528fa37bafd154f6d721442f
Riddhima <riddhima2951@gmail.com>
2026-03-12 19:57:34 +0500

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition((position) =>{
+  const { latitude, longitude } = position.coords;
+  socket.emit("send-location", { latitude, longitude} );
+
+},
+ (error) => {
+  console.log(error);
+ },
+ {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge:0
+   }
+  );
+ }
+
+ const map = L.map("map").setView([0,0], 10);  //Asking for location
+
+ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "openStreetMap"
+ }).addTo(map)
+
+
+ const markers = {};
+
+ socket.on("receive-location", (data)=>{
+  const{id, latitude, longitude} =data;
+  map.setView([latitude, longitude], 16);
+  if(markers[id]){
+    markers[id].setLatLng([latitude, longitude]);
+  }
+  else{
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+ });
+
+
+ socket.on("user-disconnected", () => {
+  if(marker[id]) {
+    map.removeLayer(marker[id]);
+    delete markers[id];
+  }
+ })
